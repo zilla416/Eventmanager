8f72cde8ab664b973cd19ffc4db127b5e23f1d84
230752 <230752@student.glu.nl>
2025-09-16 12:00:17 +0200

    project opgezet

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
+import { createApp } from 'vue';
 import './bootstrap';
+
+const test = createApp({
+    data() {
+        return {
+            message: "123456"
+        }
+    }
+});
+
+test.mount('#test');
