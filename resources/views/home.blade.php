944d7773d2b1a13d894fdd872214ee6714a313f0
230752 <230752@student.glu.nl>
2025-09-30 09:52:45 +0200

    Update home page layout: add event details section and improve structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,16 @@
 @section('title', 'Home Page')
 
 @section('content')
-<h1>Homepage</h1>
 <div class="flex flex-col">
     <a href="{{  route('loginpage') }}">Login page</a>
+    <div class="border border-gray-300 max-w-2xl mx-auto p-20">
+        <img src="" alt="">
+        <h1>event title</h1>
+        <p>locatie</p>
+        <p>datum</p>
+        <p>tijd</p>
+        <p>prijs</p>
+        <p>beschikbaarheid</p>
+    </div>
 </div>
 @endsection
