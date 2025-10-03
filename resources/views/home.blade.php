945385e3896660ab916c35328e79bb9d52332fdf
230752 <230752@student.glu.nl>
2025-10-03 11:30:00 +0200

    Refactor header and home page: improve layout structure, enhance accessibility, and update event display logic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,26 +4,112 @@
 
 @section('content')
     <div class="flex flex-col">
-        <div>
-            
-        </div>
-        <img class="h-100 object-cover" src="https://images.unsplash.com/photo-1740459057005-65f000db582f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjb25jZXJ0JTIwc3RhZ2UlMjBsaWdodHN8ZW58MXx8fHwxNzU4NzgyMzg1fDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral"
-            alt="">
+        <section id="hero-section">
 
-        @if($event)
-            <div class="border border-gray-300 max-w-2xl mx-auto p-20">
-                <img src="{{ Vite::asset($event->image) }}" alt="{{ $event->title }}" class="mb-4">
-                <h1 class="text-2xl font-bold mb-2">{{ $event->title }}</h1>
-                <p><strong>Location:</strong> {{ $event->location }}</p>
-                <p><strong>Adress:</strong> {{ $event->adress }}</p>
-                <p><strong>Date:</strong> {{ \Carbon\Carbon::parse($event->date)->translatedFormat('d F Y') }}</p>
-                <p><strong>Time:</strong> {{ \Carbon\Carbon::parse($event->time)->format('H:i') }}</p>
-                <p><strong>Price:</strong> €--</p>
-                <p><strong>Availability:</strong> {{ $event->available_spots }}</p>
-                <p class="mt-4">{{ $event->description }}</p>
+        </section>
+        <img class="h-100 object-cover"
+            src="https://images.unsplash.com/photo-1740459057005-65f000db582f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjb25jZXJ0JTIwc3RhZ2UlMjBsaWdodHN8ZW58MXx8fHwxNzU4NzgyMzg1fDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral"
+            alt="">
+        <section id="events-section" class="flex flex-col gap-5 my-12">
+            <div class="">
+                <h2 class="font-bold text-3xl">Discover Events</h2>
+                <p class="text-black/60">Find the perfect event for you</p>
             </div>
-        @else
-            <p class="text-center mt-10">No events found.</p>
-        @endif
+            <div class="flex flex-row items-center w-full px-2 h-10 bg-gray-100 rounded-xl">
+                <Button>
+                    All Events
+                </Button>
+            </div>
+            @if($event)
+                <div class="flex flex-row flex-wrap gap-4">
+                    <div class="border border-gray-200 shadow-sm rounded-xl w-3/10 h-100 mx-auto">
+                        <img src="{{ Vite::asset($event->image) }}" alt="{{ $event->title }}"
+                            class="w-full rounded-tl-xl rounded-tr-xl">
+                        <div id="event-content" class="px-4">
+                            <p class="font-semibold text-xl">{{ $event->title }}</p>
+                            {{-- <p><strong>Location:</strong> {{ $event->location }}</p> --}}
+                            {{-- <p><strong>Adress:</strong> {{ $event->adress }}</p>
+                            <p><strong>Date:</strong> {{ \Carbon\Carbon::parse($event->date)->translatedFormat('d F Y') }}</p>
+                            <p><strong>Time:</strong> {{ \Carbon\Carbon::parse($event->time)->format('H:i') }}</p>
+                            <p><strong>Price:</strong> €--</p>
+                            <p class="mt-4">{{ $event->description }}</p> --}}
+                            {{-- <p><strong>Availability:</strong> {{ $event->available_spots }}</p> --}}
+                        </div>
+                    </div>
+                    <div class="border border-gray-200 shadow-sm rounded-xl w-3/10 h-100 mx-auto">
+                        <img src="{{ Vite::asset($event->image) }}" alt="{{ $event->title }}"
+                            class="w-full rounded-tl-xl rounded-tr-xl">
+                        <div id="event-content" class="px-4">
+                            <p class="font-semibold text-xl">{{ $event->title }}</p>
+                            {{-- <p><strong>Location:</strong> {{ $event->location }}</p> --}}
+                            {{-- <p><strong>Adress:</strong> {{ $event->adress }}</p>
+                            <p><strong>Date:</strong> {{ \Carbon\Carbon::parse($event->date)->translatedFormat('d F Y') }}</p>
+                            <p><strong>Time:</strong> {{ \Carbon\Carbon::parse($event->time)->format('H:i') }}</p>
+                            <p><strong>Price:</strong> €--</p>
+                            <p class="mt-4">{{ $event->description }}</p> --}}
+                            {{-- <p><strong>Availability:</strong> {{ $event->available_spots }}</p> --}}
+                        </div>
+                    </div>
+                    <div class="border border-gray-200 shadow-sm rounded-xl w-3/10 h-100 mx-auto">
+                        <img src="{{ Vite::asset($event->image) }}" alt="{{ $event->title }}"
+                            class="w-full rounded-tl-xl rounded-tr-xl">
+                        <div id="event-content" class="px-4">
+                            <p class="font-semibold text-xl">{{ $event->title }}</p>
+                            {{-- <p><strong>Location:</strong> {{ $event->location }}</p> --}}
+                            {{-- <p><strong>Adress:</strong> {{ $event->adress }}</p>
+                            <p><strong>Date:</strong> {{ \Carbon\Carbon::parse($event->date)->translatedFormat('d F Y') }}</p>
+                            <p><strong>Time:</strong> {{ \Carbon\Carbon::parse($event->time)->format('H:i') }}</p>
+                            <p><strong>Price:</strong> €--</p>
+                            <p class="mt-4">{{ $event->description }}</p> --}}
+                            {{-- <p><strong>Availability:</strong> {{ $event->available_spots }}</p> --}}
+                        </div>
+                    </div>
+                    <div class="border border-gray-200 shadow-sm rounded-xl w-3/10 h-100 mx-auto">
+                        <img src="{{ Vite::asset($event->image) }}" alt="{{ $event->title }}"
+                            class="w-full rounded-tl-xl rounded-tr-xl">
+                        <div id="event-content" class="px-4">
+                            <p class="font-semibold text-xl">{{ $event->title }}</p>
+                            {{-- <p><strong>Location:</strong> {{ $event->location }}</p> --}}
+                            {{-- <p><strong>Adress:</strong> {{ $event->adress }}</p>
+                            <p><strong>Date:</strong> {{ \Carbon\Carbon::parse($event->date)->translatedFormat('d F Y') }}</p>
+                            <p><strong>Time:</strong> {{ \Carbon\Carbon::parse($event->time)->format('H:i') }}</p>
+                            <p><strong>Price:</strong> €--</p>
+                            <p class="mt-4">{{ $event->description }}</p> --}}
+                            {{-- <p><strong>Availability:</strong> {{ $event->available_spots }}</p> --}}
+                        </div>
+                    </div>
+                    <div class="border border-gray-200 shadow-sm rounded-xl w-3/10 h-100 mx-auto">
+                        <img src="{{ Vite::asset($event->image) }}" alt="{{ $event->title }}"
+                            class="w-full rounded-tl-xl rounded-tr-xl">
+                        <div id="event-content" class="px-4">
+                            <p class="font-semibold text-xl">{{ $event->title }}</p>
+                            {{-- <p><strong>Location:</strong> {{ $event->location }}</p> --}}
+                            {{-- <p><strong>Adress:</strong> {{ $event->adress }}</p>
+                            <p><strong>Date:</strong> {{ \Carbon\Carbon::parse($event->date)->translatedFormat('d F Y') }}</p>
+                            <p><strong>Time:</strong> {{ \Carbon\Carbon::parse($event->time)->format('H:i') }}</p>
+                            <p><strong>Price:</strong> €--</p>
+                            <p class="mt-4">{{ $event->description }}</p> --}}
+                            {{-- <p><strong>Availability:</strong> {{ $event->available_spots }}</p> --}}
+                        </div>
+                    </div>
+                    <div class="border border-gray-200 shadow-sm rounded-xl w-3/10 h-100 mx-auto">
+                        <img src="{{ Vite::asset($event->image) }}" alt="{{ $event->title }}"
+                            class="w-full rounded-tl-xl rounded-tr-xl">
+                        <div id="event-content" class="px-4">
+                            <p class="font-semibold text-xl">{{ $event->title }}</p>
+                            {{-- <p><strong>Location:</strong> {{ $event->location }}</p> --}}
+                            {{-- <p><strong>Adress:</strong> {{ $event->adress }}</p>
+                            <p><strong>Date:</strong> {{ \Carbon\Carbon::parse($event->date)->translatedFormat('d F Y') }}</p>
+                            <p><strong>Time:</strong> {{ \Carbon\Carbon::parse($event->time)->format('H:i') }}</p>
+                            <p><strong>Price:</strong> €--</p>
+                            <p class="mt-4">{{ $event->description }}</p> --}}
+                            {{-- <p><strong>Availability:</strong> {{ $event->available_spots }}</p> --}}
+                        </div>
+                    </div>
+                </div>
+            @else
+                <p class="text-center mt-10">No events found.</p>
+            @endif
+        </section>
     </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,20 +1,20 @@
 <header class="flex flex-col">
     <div class="pt-6 pb-3 flex flex-row justify-between items-center border-b border-gray-200">
         <h1 class="text-blue-600 font-bold text-3xl">EventManager</h1>
-        <div class="flex flew-row items-center gap-3 border border-gray-300 rounded-lg text-md pl-4 pr-10 py-2 w-2xl">
+        <div onmousedown="event.preventDefault()" onclick="document.getElementById('search-input').focus();" class="flex flew-row items-center gap-3 border border-gray-300 rounded-lg text-md pl-4 pr-10 py-2 w-2xl">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="size-6 text-gray-400">
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
-            <input class="outline-none w-full" type="text" placeholder="Search for events, artists, teams, and more">
+            <input id="search-input" class="outline-none w-full" type="text" placeholder="Search for events, artists, teams, and more">
         </div>
         <div id="header-buttons" class="flex flex-row items-center gap-2 font-semibold">
-            <a class="flex flex-row items-center gap-3 py-2 px-4 rounded-lg hover:bg-gray-200" href=""><img
+            <a class="flex flex-row items-center gap-3 py-2 px-4 rounded-lg duration-300 transition hover:bg-gray-200" href=""><img
                     class="w-5 h-5" src="{{ Vite::asset('resources/img/location-icon.png') }}" alt="">Utrecht,
                 NL</a>
-            <a class="py-2 px-4 rounded-lg hover:bg-gray-200" href="{{ route('register') }}">Sign In</a>
-            <a class="flex flex-row items-center gap-3 bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
+            <a class="py-2 px-4 rounded-lg duration-300 transition hover:bg-gray-200" href="{{ route('register') }}">Sign In</a>
+            <a class="flex flex-row items-center gap-3 bg-blue-600 text-white py-2 px-4 rounded-lg duration-300 transition hover:bg-blue-700"
                 href="{{  route('loginpage') }}"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                     stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -24,11 +24,11 @@
         </div>
     </div>
     <nav class="flex flex-row items-center gap-6 py-3 font-medium">
-        <a class="hover:text-blue-600" href="#">Sports</a>
-        <a class="hover:text-blue-600" href="#">Concerts</a>
-        <a class="hover:text-blue-600" href="#">Theater</a>
-        <a class="hover:text-blue-600" href="#">Comedy</a>
-        <a class="hover:text-blue-600" href="#">Family</a>
-        <a class="hover:text-blue-600" href="#">Sell Tickets</a>
+        <a class="duration-100 transition hover:text-blue-600" href="#">Sports</a>
+        <a class="duration-100 transition hover:text-blue-600" href="#">Concerts</a>
+        <a class="duration-100 transition hover:text-blue-600" href="#">Theater</a>
+        <a class="duration-100 transition hover:text-blue-600" href="#">Comedy</a>
+        <a class="duration-100 transition hover:text-blue-600" href="#">Family</a>
+        <a class="duration-100 transition hover:text-blue-600" href="#">Sell Tickets</a>
     </nav>
 </header>
