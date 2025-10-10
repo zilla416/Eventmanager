d45fc870da83144e0096baedab2104d0ec3d26f0
Rick <144334803+8-Click@users.noreply.github.com>
2025-10-10 09:38:43 +0200

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
 
-Route::get('/', function () {
+Route::get('/home', function () {
     $event = Event::first(); // just grabs the first event in DB
     return view('home', compact('event'));
 })->name('homepage');
